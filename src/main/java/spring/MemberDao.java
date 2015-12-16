b4c927b726999442a1d9aada8484c12ef6b12107
song <netsong7@naver.com>
2015-12-16 09:35:02 +0900

    작업중..

--- src/main/java/spring/MemberDao.java
+++ src/main/java/spring/MemberDao.java
@@ -1,11 +1,15 @@
 package spring;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.Collection;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
 
 public class MemberDao {
@@ -35,9 +39,18 @@ public class MemberDao {
 		}
 	}
 
-	public void insert(Member member){
-		member.setId(++nextId);
-		//map.put(member.getEmail(), member);
+	public void insert(final Member member){
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				String sql = "insert into MEMBER(EMAIL, PASSWORD, NAME, REGDATE) values(?, ?, ?, ?)";
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, member.getEmail());
+				pstmt.setString(2, member.getPassword());
+				pstmt.setString(3, member.getName());
+				pstmt.setTimestamp(4, new Timestamp(member.getRegisterDate().getTime()));
+				return pstmt;
+			}
+		}, 두번째 인자);
 	}
 	
 	public void update(Member member){
